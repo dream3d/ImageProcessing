83c7c1becee063863e000a901875ccf8edf8c949
Michael Jackson <mike.jackson@bluequartz.net>
2016-02-19 09:18:59 -0600

    Updating copyright to incude 2016

--- ImageProcessingFilters/ItkImportImageStack.h
+++ ImageProcessingFilters/ItkImportImageStack.h
@@ -1,6 +1,6 @@
 /* ============================================================================
 * Copyright (c) 2014 William Lenthe
-* Copyright (c) 2009-2015 BlueQuartz Software, LLC
+* Copyright (c) 2009-2016 BlueQuartz Software, LLC
 *
 * Redistribution and use in source and binary forms, with or without modification,
 * are permitted provided that the following conditions are met:
