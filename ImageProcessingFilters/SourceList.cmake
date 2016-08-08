b32aac04ca684cc88ed0449ea12b5d8f76fa478a
Alexander Woodward <alex.woodward96@gmail.com>
2016-08-08 13:54:31 -0400

    Removing ImportImageMontage filter

--- ImageProcessingFilters/SourceList.cmake
+++ ImageProcessingFilters/SourceList.cmake
@@ -19,7 +19,6 @@ SIMPL_START_FILTER_GROUP(
 # List your public filters here
 set(_PublicFilters
   #AlignSectionsPhaseCorrelation
-  ImportRegisteredImageMontage
   ItkAutoThreshold
   ItkBinaryWatershedLabeled
   ItkConvertArrayTo8BitImage
