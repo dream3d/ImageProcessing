5f3f13f192e07ae832c42e942fa499a923e8c5c8
Michael Jackson <mike.jackson@bluequartz.net>
2018-07-06 09:55:45 -0400

    Add include guard in the form of pragma statement

--- ImageProcessingHelpers.hpp
+++ ImageProcessingHelpers.hpp
@@ -29,6 +29,7 @@
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+#pragma once
 
 #include <limits>
 #include "itkImage.h"
